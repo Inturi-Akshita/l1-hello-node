fc13f0493440c80e6b797fa5eede0398abcfa6ff
Inturi-Akshita <inturiakshita@gmail.com>
2022-11-22 19:50:58 +0530

    CLI practiced

---
+++ CLI.js
@@ -0,0 +1,20 @@
+// const readline = require('readline')
+// const lineDetail = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// })
+// lineDetail.question('Please provide your name - ', (name) => {
+//   console.log(`Hi ${name}!`)
+//   lineDetail.close()
+// })
+
+const args = require('minimist')(process.argv.slice(2), {
+  alias: {
+    n: 'name',
+    a: 'age'
+  },
+  default: {
+    greeting: 'Hello'
+  }
+})
+console.log(args)
